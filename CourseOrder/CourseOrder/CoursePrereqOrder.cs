ae60fb9ffb8588e28fe7effe90b629a939150fe0
Tara Jorgensen <jorgensen.tara@gmail.com>
2015-09-02 08:51:26 -0600

    Replaced Test File, added Recursion

--- CourseOrder/CourseOrder/CoursePrereqOrder.cs
+++ CourseOrder/CourseOrder/CoursePrereqOrder.cs
@@ -15,8 +15,10 @@ namespace CourseOrder
         {
             string test = @"Introduction to Paper Airplanes: 
 Advanced Throwing Techniques: Introduction to Paper Airplanes
+TestOne: Introduction to Paper Airplanes
+TestTwo: TestOne
 History of Cubicle Siege Engines: Rubber Band Catapults 101
-Advanced Office Warfare: History of Cubicle Siege Engines 
+Advanced Office Warfare: History of Cubicle Siege Engines
 Rubber Band Catapults 101: 
 Paper Jet Engines: Introduction to Paper Airplanes";
 
@@ -24,17 +26,28 @@ Paper Jet Engines: Introduction to Paper Airplanes";
             //string userInput = Console.ReadLine();
 
             var splitCourses = CourseSplit(test);
-            List<string> finalOrder = new List<string>();
-            foreach (string[] course in splitCourses)
+            List<string> finalOrderDuplicates = new List<string>();
+            foreach(string[] course in splitCourses)
             {
-                GetPrerequisite(splitCourses, course[0]);
+                string prerequisite = GetPrerequisite(splitCourses, course[0]);
+                if (prerequisite != "")
+                {
+                    finalOrderDuplicates.Add(prerequisite);
+                    finalOrderDuplicates.Add(course[0]);
+                }
+                else
+                {
+                    finalOrderDuplicates.Insert(0,course[0]);
+                }
             }
+            List<string> FinalOrdernoDuplicates = finalOrderDuplicates.Distinct().ToList();
+        Console.ReadLine();
         }
+  
 
         public static string[][] CourseSplit(string input)
         {
             return Regex.Split(input, "\r\n").Select(t => Regex.Split(t, ": ")).ToArray();
-            //List<string> courseGroup = new List<string>(courses);
         }
 
         public static string GetPrerequisite(string[][] courses, string course)
@@ -45,7 +58,12 @@ Paper Jet Engines: Introduction to Paper Airplanes";
                 if (item[0] == course && item[1] != "")
                 {
                     prerequisite = item[1];
+                    GetPrerequisite(courses, prerequisite);
                 }
+            }
+            if(course == prerequisite)
+            {
+                throw new Exception();
             }
             return prerequisite;
         }
