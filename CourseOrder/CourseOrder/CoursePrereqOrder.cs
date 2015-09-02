2f1938f378ade1a283684a21003fffa4fe0d1587
Tara Jorgensen <jorgensen.tara@gmail.com>
2015-09-01 19:51:02 -0600

    HasPrerequisiteTest added

--- CourseOrder/CourseOrder/CoursePrereqOrder.cs
+++ CourseOrder/CourseOrder/CoursePrereqOrder.cs
@@ -9,42 +9,45 @@ using System.Threading.Tasks;
 
 namespace CourseOrder
 {
-  public class CoursePrereqOrder
-  {
-    public static void Main()
+    public class CoursePrereqOrder
     {
-      string test = @"Introduction to Paper Airplanes: 
+        public static void Main()
+        {
+            string test = @"Introduction to Paper Airplanes: 
 Advanced Throwing Techniques: Introduction to Paper Airplanes
 History of Cubicle Siege Engines: Rubber Band Catapults 101
 Advanced Office Warfare: History of Cubicle Siege Engines 
 Rubber Band Catapults 101: 
 Paper Jet Engines: Introduction to Paper Airplanes";
 
-      //test = test.Replace("\n", "");
-      //Console.WriteLine("Please enter your list of courses");
-      //string userInput = Console.ReadLine();
-      var splitCourses = CourseSplit(test);
-      foreach (string[] course in splitCourses)
-      {
-        Swap(ref course[0], ref course[1]);
-      }
+            //test = test.Replace("\n", "");
+            //Console.WriteLine("Please enter your list of courses");
+            //string userInput = Console.ReadLine();
+            var splitCourses = CourseSplit(test);
+            foreach (string[] course in splitCourses)
+            {
+                Swap(ref course[0], ref course[1]);
+            }
+        }
 
-      Console.ReadLine();
+        public static string[][] CourseSplit(string input)
+        {
+            return Regex.Split(input, "\r\n").Select(t => Regex.Split(t, ": ")).ToArray();
+            //List<string> courseGroup = new List<string>(courses);
+        }
 
-    }
-    public static string[][] CourseSplit(string input)
-    {
-      return Regex.Split(input, "\r\n").Select(t =>  Regex.Split(t, ": ")).ToArray();
-      //List<string> courseGroup = new List<string>(courses);
-    }
+        public static void Swap(ref string a, ref string b)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
 
-    public static void Swap(ref string a, ref string b)
-    {
-      string temp = a;
-      a = b;
-      b = temp;
+        public static bool HasPrerequisite(ref string course)
+        {
+            throw new NotImplementedException();
+        }
     }
-  }
 }
 
 
--- CourseOrder/CoursePrereqOrderTests/OrderTests.cs
+++ CourseOrder/CoursePrereqOrderTests/OrderTests.cs
@@ -9,30 +9,39 @@ using CourseOrder;
 
 namespace CoursePrereqOrderTests
 {
-  [TestFixture]
-  public class OrderTests
-  {
-    [Test]
-    public void TestCourseSplit()
+    [TestFixture]
+    public class OrderTests
     {
-      string test = @"Introduction to Paper Airplanes: 
+        [Test]
+        public void TestCourseSplit()
+        {
+            string test = @"Introduction to Paper Airplanes: 
 Advanced Throwing Techniques: Introduction to Paper Airplanes";
-      string[][] array =CoursePrereqOrder.CourseSplit(test);
-      Assert.AreEqual(array[0][0], "Introduction to Paper Airplanes" );
-      Assert.AreEqual(array[0][1], "" );
-      Assert.AreEqual(array[1][0], "Advanced Throwing Techniques");
-      Assert.AreEqual(array[1][1], "Introduction to Paper Airplanes");
-    }
+            string[][] array = CoursePrereqOrder.CourseSplit(test);
+            Assert.AreEqual(array[0][0], "Introduction to Paper Airplanes");
+            Assert.AreEqual(array[0][1], "");
+            Assert.AreEqual(array[1][0], "Advanced Throwing Techniques");
+            Assert.AreEqual(array[1][1], "Introduction to Paper Airplanes");
+        }
 
-    [Test]
-    public void TestSwap()
-    {
-      string[] test = new string[2];
-      test[0] = "a";
-      test[1] = "b";
-      CoursePrereqOrder.Swap(ref test[0], ref test[1]);
-      Assert.AreEqual("a", test[1]);
-      Assert.AreEqual("b", test[0]);
+        [Test]
+        public void TestSwap()
+        {
+            string[] test = new string[2];
+            test[0] = "a";
+            test[1] = "b";
+            CoursePrereqOrder.Swap(ref test[0], ref test[1]);
+            Assert.AreEqual("a", test[1]);
+            Assert.AreEqual("b", test[0]);
+        }
+        [Test]
+        public void TestHasPrerequisite()
+        {
+            string[] test = new string[2];
+            test[0] = "a";
+            test[1] = "b";
+            Assert.AreEqual(true, CoursePrereqOrder.HasPrerequisite(ref test[1]));
+
+        }
     }
-  }
 }
