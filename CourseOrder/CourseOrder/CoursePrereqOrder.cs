26db34b01fef35bede20629dacb4bff2a05c5beb
Tara Jorgensen <jorgensen.tara@gmail.com>
2015-09-01 18:21:56 -0600

    1st add

---
+++ CourseOrder/CourseOrder/CoursePrereqOrder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+
+namespace CourseOrder
+{
+  public class CoursePrereqOrder
+  {
+    public static void Main()
+    {
+      string test = @"Introduction to Paper Airplanes: 
+Advanced Throwing Techniques: Introduction to Paper Airplanes
+History of Cubicle Siege Engines: Rubber Band Catapults 101
+Advanced Office Warfare: History of Cubicle Siege Engines 
+Rubber Band Catapults 101: 
+Paper Jet Engines: Introduction to Paper Airplanes";
+
+      //test = test.Replace("\n", "");
+      //Console.WriteLine("Please enter your list of courses");
+      //string userInput = Console.ReadLine();
+      var splitCourses = CourseSplit(test);
+      foreach (string[] course in splitCourses)
+      {
+        Swap(ref course[0], ref course[1]);
+      }
+
+      Console.ReadLine();
+
+    }
+    public static string[][] CourseSplit(string input)
+    {
+      return Regex.Split(input, "\r\n").Select(t =>  Regex.Split(t, ": ")).ToArray();
+      //List<string> courseGroup = new List<string>(courses);
+    }
+
+    public static void Swap(ref string a, ref string b)
+    {
+      string temp = a;
+      a = b;
+      b = temp;
+    }
+  }
+}
+
+
+
+
+//  foreach (string n in s)
+//  {
+//    Console.WriteLine(n + ',');
+
+//  }
+//  Console.WriteLine(s);
+//}
+//return String.Empty;
---
+++ CourseOrder/CoursePrereqOrderTests/OrderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NUnit.Framework;
+using CourseOrder;
+
+namespace CoursePrereqOrderTests
+{
+  [TestFixture]
+  public class OrderTests
+  {
+    [Test]
+    public void TestCourseSplit()
+    {
+      string test = @"Introduction to Paper Airplanes: 
+Advanced Throwing Techniques: Introduction to Paper Airplanes";
+      string[][] array =CoursePrereqOrder.CourseSplit(test);
+      Assert.AreEqual(array[0][0], "Introduction to Paper Airplanes" );
+      Assert.AreEqual(array[0][1], "" );
+      Assert.AreEqual(array[1][0], "Advanced Throwing Techniques");
+      Assert.AreEqual(array[1][1], "Introduction to Paper Airplanes");
+    }
+
+    [Test]
+    public void TestSwap()
+    {
+      string[] test = new string[2];
+      test[0] = "a";
+      test[1] = "b";
+      CoursePrereqOrder.Swap(ref test[0], ref test[1]);
+      Assert.AreEqual("a", test[1]);
+      Assert.AreEqual("b", test[0]);
+    }
+  }
+}
